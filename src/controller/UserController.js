7a279eba944a01ad68301966b28d4a09c7a6b9a6
unknown <alber725@gmail.com>
2020-05-11 18:48:41 -0300

    CRUD usuário concluído

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -13,5 +13,43 @@ module.exports = {
         const user = await User.create({ name, email });
 
         return res.json(user);
+    },
+
+    async update(req, res, next) {     
+
+        const { name, email } = req.body;
+        const { user_id } = req.params;
+
+        const userId = await User.findByPk(user_id);
+
+        if (!userId) {
+            return res.status(400).json({ error: 'User not found' });
+        };
+
+        const user = await User.update(
+            {name, email},
+            {where: { id: user_id }}
+        );
+
+        return res.json({name, email});
+
+    },
+
+    async delete(req, res, next) {
+
+        const { user_id } = req.params
+
+        const userId = await User.findByPk(user_id);
+
+        if (!userId) {
+            return res.status(400).json({ error: 'User not found' });
+        };
+
+        const user = await User.destroy({
+            where: { id: user_id }
+        });
+
+        return res.send('');
+
     }
 };
